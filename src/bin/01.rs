54f59c688876d6d3dcf7f3867da69a2cba16e626
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2023-12-05 19:35:00 +0200

    day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,11 +1,65 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines().clone();
+
+    let mut result = 0;
+    for line in lines.into_iter() {
+        let mut iter = line.chars().filter(|c| c.is_ascii_digit());
+        let first = iter.next().unwrap().to_digit(10).unwrap();
+        let last = match iter.last() {
+            Some(l) => l.to_digit(10).unwrap(),
+            None => first,
+        };
+        println!("{}{}", first, last);
+        result = result + first * 10 + last;
+    }
+
+    Some(result)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let lines = input.lines().clone();
+
+    let mut result = 0;
+    for line in lines.into_iter() {
+        let mut it = (0..line.len()).filter_map(|index| {
+            let reduced_line = &line[index..];
+            let result = if reduced_line.starts_with("one") {
+                '1'
+            } else if reduced_line.starts_with("two") {
+                '2'
+            } else if reduced_line.starts_with("three") {
+                '3'
+            } else if reduced_line.starts_with("four") {
+                '4'
+            } else if reduced_line.starts_with("five") {
+                '5'
+            } else if reduced_line.starts_with("six") {
+                '6'
+            } else if reduced_line.starts_with("seven") {
+                '7'
+            } else if reduced_line.starts_with("eight") {
+                '8'
+            } else if reduced_line.starts_with("nine") {
+                '9'
+            } else {
+                reduced_line.chars().next().unwrap()
+            };
+
+            result.to_digit(10)
+        });
+
+        let first = it.next().unwrap();
+        let last = match it.last() {
+            Some(l) => l,
+            None => first,
+        };
+        result = result + first * 10 + last;
+        println!("{}{}", first, last);
+    }
+
+    Some(result)
 }
 
 #[cfg(test)]
